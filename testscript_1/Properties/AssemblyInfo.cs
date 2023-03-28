c6b00c5ddc9f15b26d37951d364b3bc3e7f0e7a0
pdruwel <pieter.druwel@telenetgroup.be>
2023-03-28 14:23:42 +0200

    Add project files.

---
+++ testscript_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("testscript_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("testscript_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("5CFDA9DD-1F8F-4FBF-983C-93C9B3F059CA")]
+[assembly: AssemblyVersion("1.0.0.0")]
